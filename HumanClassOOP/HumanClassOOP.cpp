eda3e2faf9f6b9221d6d8975e1a827249716c2c5
Рома Тищенко <Romaha457@gmail.com>
2024-01-07 19:40:52 +1000

    Шаг 2

--- HumanClassOOP/HumanClassOOP.cpp
+++ HumanClassOOP/HumanClassOOP.cpp
@@ -2,13 +2,20 @@
 
 class Human {
 public:
-
+    Human(std::string name, bool gender, int age) {
+        this->name = name;
+        this->gender = gender;
+        this->age = age;
+    }
 private:
-
+    std::string name;
+    bool gender; //0 - male, 1 - female
+    int age;
 };
 
 
 int main()
 {
-    Human Adam; Human Eve;
+    Human first("Adam", 0, 25);
+    Human second("Eve", 1, 20);
 }
